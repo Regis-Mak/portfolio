1cc98f3873ab49c8e5ecddd37ad9f2eafb1d300d
Regis Mak <regmak672@gmail.com>
2025-05-21 02:19:20 -0700

    First Half

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,25 @@
+import { ThemeToggle } from "../components/ThemeToggle"
+import { StarBackground } from "../components/StarBackground"
+import { Navbar } from "../components/NavBar"
+import { HeroSection } from "../components/HeroSection"
+import { AboutSection } from "../components/AboutSection"
+
 export const Home = () => {
-    return <div>
-        
+    return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
         {/* Theme Toggle */}
+        <ThemeToggle/>
 
         {/* Background Efects */}
+        <StarBackground/>
+
+        {/* Navbar */}
+        <Navbar/>
+
+        {/* Main Content */}
+        <main>
+            <HeroSection/>
+            <AboutSection/>
+        </main>
+        {/* Footer */}
     </div>
 }
