68100557bd849371479c97c661f46ac91dc2295b
Regis Mak <regmak672@gmail.com>
2025-05-21 23:52:28 -0700

    Base Done?

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,3 +1,4 @@
+import { ArrowRight, ExternalLink, Github } from "lucide-react";
 
 const projects = [
     {
@@ -48,10 +49,58 @@ export const ProjectsSection = () => {
                 {projects.map((project, key) => (
                     <div key={key} className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover">
                         <div className="h-48 overflow-hidden">
-                            <img src={project.image} alt={project.title} className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"/>
+                            <img 
+                            src={project.image} 
+                            alt={project.title} 
+                            className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
+                            />
                         </div>
+
+                        <div className="p-6">
+                            <div className="flex flex-wrap gap-2 mb-4">
+                                {project.tags.map((tag) => (
+                                    <span className="px-2 py-1 text-xs font-medium border rounded-full bg-secondary text-secondary-foreground">
+                                        {tag}
+                                    </span>
+                                ))}
+                            </div>
+                        
+
+                            <h3 className="font-xl font-semibold mb-1">{project.title}</h3>
+                            <p className="text-muted-foreground text-sm mb-4">{project.description}</p>
+
+                            <div className="flex justify-between items-center">
+                                <div className="flex space-x-3">
+                                    <a 
+                                    href={project.demoUrl} 
+                                    target="_blank"
+                                    className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                                    >
+                                        <ExternalLink size={20}/>
+                                    </a>
+                                    <a 
+                                    href={project.githubUrl} 
+                                    target="_blank"
+                                    className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                                    >
+                                        <Github size={20}/>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        
                     </div>
                 ))}
+            </div>
+
+            <div className="text-center mt-12">
+                <a 
+                className="cosmic-button w-fit flex items-center mx-auto gap-2" 
+                target="_blank"
+                href="https://github.com/Regis-Mak"
+                >
+                    Check My Github <ArrowRight size={16}/>
+                </a>
             </div>
         </div>
     </section>
