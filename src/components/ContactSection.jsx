a868262199240437c4e30aafc28a7eb8e7a3639d
Regis Mak <regmak672@gmail.com>
2025-05-26 17:35:18 -0700

    Update ContactSection.jsx

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -52,7 +52,7 @@ export const ContactSection = () => {
             </div>
 
             <div className="pt-8">
-              <h4 className="font-medium mb-4"> Connect With Me !</h4>
+              <h4 className="font-medium mb-4"> Connect With Me</h4>
               <div className="flex space-x-4 justify-center">
                 <a href="https://www.linkedin.com/in/regismak/" target="_blank">
                   <Linkedin />
