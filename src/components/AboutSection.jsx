72c754b00a557099b4bc1378178221a457c6dd2e
Regis Mak <regmak672@gmail.com>
2025-08-16 15:42:22 -0700

    updates to About

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -27,7 +27,7 @@ export const AboutSection = () => {
                             Get In Touch
                         </a>
 
-                        <a href="www.regismak.com/coming-soon" className="px-6 py-2 rounded-full border border-primary hover:bg-primary/10 transition-colors duration-300">
+                        <a target = "_blank" href="https://drive.google.com/file/d/1_VB1p5dIdenuJA_8FVNIKXXTkcpuJzDa/view" className="px-6 py-2 rounded-full border border-primary hover:bg-primary/10 transition-colors duration-300">
                             Resume
                         </a>
                     </div>
